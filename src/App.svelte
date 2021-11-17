72834d86e18e261a4d250d1c800b49d1b7544055
Riza Fahmi <rizafahmi@gmail.com>
2021-11-17 09:00:50 +0700

    Finish svelte compile to web components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,33 @@
 <script>
-	export let name;
+import './Counter.svelte'
 </script>
 
+<svelte:options tag="web-components" />
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Counter</h1>
+    <c-ounter />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        display: grid;
+        place-items: center;
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
---
+++ src/Counter.svelte
@@ -0,0 +1,46 @@
+<script>
+ export let value = 0;
+ let digit=value;
+
+ function increment() {
+     digit = digit + 1;
+ }
+ function decrement() {
+     digit = digit - 1;
+ }
+</script>
+
+<svelte:options tag="c-ounter" />
+<div class="counter">
+    <button class="decrement" on:click={decrement}>-</button>
+    <span class="digit">{digit}</span>
+    <button class="increment" on:click={increment}>+</button>
+</div>
+
+<style>
+ .counter {
+     font-size: 1.5em;
+     display: flex;
+     margin: 0.5em;
+ }
+ button {
+     border: 0;
+     width: 60px;
+     height: 50px;
+     margin: 5px;
+     font-size: 20px;
+ }
+ button.decrement {
+     background-color: #ffadad;
+ }
+ button.increment {
+     background-color: #a0c4ff;
+ }
+
+ .digit {
+     display: flex;
+     align-items: center;
+     padding-left: 0.5em;
+     padding-right: 0.5em;
+ }
+</style>
